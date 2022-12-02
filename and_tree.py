bdf2c7a3ab0a292133ed88e3c151c3766390a288
Dainternetdude <53956237+Dainternetdude@users.noreply.github.com>
2022-12-01 21:37:06 -0700

    add main loop skeleton & save function

--- and_tree.py
+++ and_tree.py
@@ -1,9 +1,16 @@
 class Node:
+    element = {}
     children = []
     parent = None
     solved = False
 
+    def __init__(self, element, parent):
+        self.element = element
+        self.parent = parent
 
 class Tree:
     root = None
     solved = False
+
+    def __init__(self, root):
+        self.root = root
---
+++ checks.py
@@ -0,0 +1,10 @@
+def check_hard_constraints(node):
+    pass
+
+
+def check_soft_constraints(node):
+    pass
+
+
+def is_complete_schedule(node):
+    pass
